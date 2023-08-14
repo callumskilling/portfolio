921a4ed4d41ee2bc20ed2be3a89e7a3e26ff279b
callumskilling <88871549+callumskilling@users.noreply.github.com>
2023-08-14 12:21:07 +1000

    added data template

---
+++ src/assets/data/portfolioData.js
@@ -0,0 +1,166 @@
+export const portfolioData = [
+    {
+        "project_title": "Project Title",
+        "project_description": "Project description will be written here",
+        "project_tags": "Tag 1 | Tag 2",
+        "language_tags": "HTML | CSS | Javascript",
+        "project_background": "Project background will be written here",
+        "image_paths": {
+            "hero": {
+                "hero_desktop": require("../images/favicon/favicon.png"),
+                "hero_desktop_2x": require("../images/favicon/favicon.png"),
+                "hero_tablet": require("../images/favicon/favicon.png"),
+                "hero_tablet_2x": require("../images/favicon/favicon.png"),
+                "hero_mobile": require("../images/favicon/favicon.png"),
+                "hero_mobile_2x": require("../images/favicon/favicon.png"),
+            },
+            "detail_hero": {
+                "hero_desktop": require("../images/favicon/favicon.png"),
+                "hero_desktop_2x": require("../images/favicon/favicon.png"),
+                "hero_tablet": require("../images/favicon/favicon.png"),
+                "hero_tablet_2x": require("../images/favicon/favicon.png"),
+                "hero_mobile": require("../images/favicon/favicon.png"),
+                "hero_mobile_2x": require("../images/favicon/favicon.png"),
+            },
+            "preview1": {
+                "preview1_desktop": require("../images/favicon/favicon.png"),
+                "preview1_desktop_2x": require("../images/favicon/favicon.png"),
+                "preview1_tablet": require("../images/favicon/favicon.png"),
+                "preview1_tablet_2x": require("../images/favicon/favicon.png"),
+                "preview1_mobile": require("../images/favicon/favicon.png"),
+                "preview1_mobile_2x": require("../images/favicon/favicon.png"),
+            },
+            "preview2": {
+                "preview2_desktop": require("../images/favicon/favicon.png"),
+                "preview2_desktop_2x": require("../images/favicon/favicon.png"),
+                "preview2_tablet": require("../images/favicon/favicon.png"),
+                "preview2_tablet_2x": require("../images/favicon/favicon.png"),
+                "preview2_mobile": require("../images/favicon/favicon.png"),
+                "preview2_mobile_2x": require("../images/favicon/favicon.png"),
+            }
+        }
+    },
+    {
+        "project_title": "Project Title",
+        "project_description": "Project description will be written here",
+        "project_tags": "Tag 1 | Tag 2",
+        "language_tags": "HTML | CSS | Javascript",
+        "project_background": "Project background will be written here",
+        "image_paths": {
+            "hero": {
+                "hero_desktop": require("../images/favicon/favicon.png"),
+                "hero_desktop_2x": require("../images/favicon/favicon.png"),
+                "hero_tablet": require("../images/favicon/favicon.png"),
+                "hero_tablet_2x": require("../images/favicon/favicon.png"),
+                "hero_mobile": require("../images/favicon/favicon.png"),
+                "hero_mobile_2x": require("../images/favicon/favicon.png"),
+            },
+            "detail_hero": {
+                "hero_desktop": require("../images/favicon/favicon.png"),
+                "hero_desktop_2x": require("../images/favicon/favicon.png"),
+                "hero_tablet": require("../images/favicon/favicon.png"),
+                "hero_tablet_2x": require("../images/favicon/favicon.png"),
+                "hero_mobile": require("../images/favicon/favicon.png"),
+                "hero_mobile_2x": require("../images/favicon/favicon.png"),
+            },
+            "preview1": {
+                "preview1_desktop": require("../images/favicon/favicon.png"),
+                "preview1_desktop_2x": require("../images/favicon/favicon.png"),
+                "preview1_tablet": require("../images/favicon/favicon.png"),
+                "preview1_tablet_2x": require("../images/favicon/favicon.png"),
+                "preview1_mobile": require("../images/favicon/favicon.png"),
+                "preview1_mobile_2x": require("../images/favicon/favicon.png"),
+            },
+            "preview2": {
+                "preview2_desktop": require("../images/favicon/favicon.png"),
+                "preview2_desktop_2x": require("../images/favicon/favicon.png"),
+                "preview2_tablet": require("../images/favicon/favicon.png"),
+                "preview2_tablet_2x": require("../images/favicon/favicon.png"),
+                "preview2_mobile": require("../images/favicon/favicon.png"),
+                "preview2_mobile_2x": require("../images/favicon/favicon.png"),
+            }
+        }
+    },
+    {
+        "project_title": "Project Title",
+        "project_description": "Project description will be written here",
+        "project_tags": "Tag 1 | Tag 2",
+        "language_tags": "HTML | CSS | Javascript",
+        "project_background": "Project background will be written here",
+        "image_paths": {
+            "hero": {
+                "hero_desktop": require("../images/favicon/favicon.png"),
+                "hero_desktop_2x": require("../images/favicon/favicon.png"),
+                "hero_tablet": require("../images/favicon/favicon.png"),
+                "hero_tablet_2x": require("../images/favicon/favicon.png"),
+                "hero_mobile": require("../images/favicon/favicon.png"),
+                "hero_mobile_2x": require("../images/favicon/favicon.png"),
+            },
+            "detail_hero": {
+                "hero_desktop": require("../images/favicon/favicon.png"),
+                "hero_desktop_2x": require("../images/favicon/favicon.png"),
+                "hero_tablet": require("../images/favicon/favicon.png"),
+                "hero_tablet_2x": require("../images/favicon/favicon.png"),
+                "hero_mobile": require("../images/favicon/favicon.png"),
+                "hero_mobile_2x": require("../images/favicon/favicon.png"),
+            },
+            "preview1": {
+                "preview1_desktop": require("../images/favicon/favicon.png"),
+                "preview1_desktop_2x": require("../images/favicon/favicon.png"),
+                "preview1_tablet": require("../images/favicon/favicon.png"),
+                "preview1_tablet_2x": require("../images/favicon/favicon.png"),
+                "preview1_mobile": require("../images/favicon/favicon.png"),
+                "preview1_mobile_2x": require("../images/favicon/favicon.png"),
+            },
+            "preview2": {
+                "preview2_desktop": require("../images/favicon/favicon.png"),
+                "preview2_desktop_2x": require("../images/favicon/favicon.png"),
+                "preview2_tablet": require("../images/favicon/favicon.png"),
+                "preview2_tablet_2x": require("../images/favicon/favicon.png"),
+                "preview2_mobile": require("../images/favicon/favicon.png"),
+                "preview2_mobile_2x": require("../images/favicon/favicon.png"),
+            }
+        }
+    },
+    {
+        "project_title": "Project Title",
+        "project_description": "Project description will be written here",
+        "project_tags": "Tag 1 | Tag 2",
+        "language_tags": "HTML | CSS | Javascript",
+        "project_background": "Project background will be written here",
+        "image_paths": {
+            "hero": {
+                "hero_desktop": require("../images/favicon/favicon.png"),
+                "hero_desktop_2x": require("../images/favicon/favicon.png"),
+                "hero_tablet": require("../images/favicon/favicon.png"),
+                "hero_tablet_2x": require("../images/favicon/favicon.png"),
+                "hero_mobile": require("../images/favicon/favicon.png"),
+                "hero_mobile_2x": require("../images/favicon/favicon.png"),
+            },
+            "detail_hero": {
+                "hero_desktop": require("../images/favicon/favicon.png"),
+                "hero_desktop_2x": require("../images/favicon/favicon.png"),
+                "hero_tablet": require("../images/favicon/favicon.png"),
+                "hero_tablet_2x": require("../images/favicon/favicon.png"),
+                "hero_mobile": require("../images/favicon/favicon.png"),
+                "hero_mobile_2x": require("../images/favicon/favicon.png"),
+            },
+            "preview1": {
+                "preview1_desktop": require("../images/favicon/favicon.png"),
+                "preview1_desktop_2x": require("../images/favicon/favicon.png"),
+                "preview1_tablet": require("../images/favicon/favicon.png"),
+                "preview1_tablet_2x": require("../images/favicon/favicon.png"),
+                "preview1_mobile": require("../images/favicon/favicon.png"),
+                "preview1_mobile_2x": require("../images/favicon/favicon.png"),
+            },
+            "preview2": {
+                "preview2_desktop": require("../images/favicon/favicon.png"),
+                "preview2_desktop_2x": require("../images/favicon/favicon.png"),
+                "preview2_tablet": require("../images/favicon/favicon.png"),
+                "preview2_tablet_2x": require("../images/favicon/favicon.png"),
+                "preview2_mobile": require("../images/favicon/favicon.png"),
+                "preview2_mobile_2x": require("../images/favicon/favicon.png"),
+            }
+        }
+    }
+]
