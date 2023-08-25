c2e984c011ff08cda8b2c170bc01d0016aee35ae
callumskilling <88871549+callumskilling@users.noreply.github.com>
2023-08-25 16:23:13 +1000

    removed unneccessary text

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,9 @@
+import { HomeHero } from "../components/HomeHero/HomeHero"
+
 export const Home = () => {
     return (
         <>
-            <h1>Callum Skilling</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat cum nostrum ipsam sapiente ipsa reprehenderit est aut dolore doloremque odio praesentium, quas accusamus facilis corrupti, quidem quo labore fugiat explicabo.</p>
+            <HomeHero />
         </>
     )
 }
