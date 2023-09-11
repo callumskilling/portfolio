aa054fb0538c9d1c7aa945be08d22778ba12c2be
callumskilling <88871549+callumskilling@users.noreply.github.com>
2023-09-11 15:58:47 +1000

    created blurb component as a starting point

--- src/components/Blurb/Blurb.jsx
+++ src/components/Blurb/Blurb.jsx
@@ -1,11 +1,17 @@
-import { Developer } from "../../assets/images/icons/Developer"
+import { TechBundle } from "../../assets/images/icons/TechBundle"
 import "./Blurb.css"
+
+
 
 
 export const Blurb = () => {
   return (
     <div id="blurb_container">
-      <Developer height="20rem" id="developer_image"/>
+      <div id="blurb_wrapper">
+        <TechBundle id="tech_bundle" />
+        <p>I build dynamic websites using HTML, CSS, and JavaScript. I have completed the Front-End Engineer career path on codecademy.com, where I learned how to apply basic design theory, use the command line, and work with front-end frameworks like React and Redux. I have also completed several projects that demonstrate my ability to create responsive, interactive, and user-friendly web applications.</p><br />
+        <p>I am passionate about learning new technologies and improving my skills as a front-end engineer. I am always looking for new challenges and opportunities to create amazing websites that solve real-world problems.</p>
+      </div>
     </div>
   )
 }
