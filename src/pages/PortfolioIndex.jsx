df4bcfd0a45b73876f7a98e068325bf6887c766a
callumskilling <88871549+callumskilling@users.noreply.github.com>
2023-09-11 15:59:29 +1000

    contained project card indexes within a div

--- src/pages/PortfolioIndex.jsx
+++ src/pages/PortfolioIndex.jsx
@@ -3,12 +3,12 @@ import { portfolioData } from '../assets/data/portfolioData';
 
 const PortfolioIndex = () => {
     return (
-        <>
+        <div id="portfolio_index_container">
             {portfolioData.map((project, index) => (
                 <ProjectCardIndex key={`${project.project_title}_${index}`} project={project} />
             )
             )}
-        </>
+        </div>
     )
 }
 
