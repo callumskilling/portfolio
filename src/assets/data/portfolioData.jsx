d0dede96b795e84ce6b5a8c9b5e5f1a59feaafc3
callumskilling <88871549+callumskilling@users.noreply.github.com>
2023-09-12 15:56:11 +1000

    turned portfolioData into a jsx file

---
+++ src/assets/data/portfolioData.jsx
@@ -0,0 +1,134 @@
+export const portfolioData = [
+    {
+        "project_title": "Reddit App",
+        "project_index": "reddit-app",
+        "project_description": <p>The goal was to create a Reddit app that allows users to view and search posts. It should also allow the user to load comments and view the popularity. In this project, I use React/Redux to store and display the results of fetching data using Reddit's API.</p>,
+        "project_tags": "REST API | React & Redux",
+        "language_tags": "HTML, CSS & Javascript | React & Redux | REST",
+        "project_background": <p>This project demonstrates my ability to work with APIs and Redux.<br />The app uses the Reddit API to fetch data and displays it in a responsive and user-friendly interface.<br />The design is very simple and and provides a read only experience of Reddit posts.<br />I really enjoyed this project, working with Redux, and learning some supplementary new skills like displaying timestamps relative to now and skeleton loading.</p>,
+        "project_code_url": "https://github.com/callumskilling/112.013_reddit_app",
+        "project_live_url": "https://callumskilling.github.io/112.013_reddit_app/",
+        "image_paths": {
+            "thumbnail": {
+                "thumbnail_desktop": "/assets/images/desktop/reddit_thumbnail_desktop.png",
+                "thumbnail_desktop_2x": "/assets/images/desktop/reddit_thumbnail_desktop_2x.png",
+                "thumbnail_tablet": "/assets/images/tablet/reddit_thumbnail_tablet.png",
+                "thumbnail_tablet_2x": "/assets/images/tablet/reddit_thumbnail_tablet_2x.png",
+                "thumbnail_mobile": "/assets/images/mobile/reddit_thumbnail_mobile.png",
+                "thumbnail_mobile_2x": "/assets/images/mobile/reddit_thumbnail_mobile_2x.png",
+            },
+            "hero": {
+                "hero_desktop": "/assets/images/desktop/reddit_hero_desktop.png",
+                "hero_desktop_2x": "/assets/images/desktop/reddit_hero_desktop_2x.png",
+                "hero_tablet": "/assets/images/tablet/reddit_hero_tablet.png",
+                "hero_tablet_2x": "/assets/images/tablet/reddit_hero_tablet_2x.png",
+                "hero_mobile": "/assets/images/mobile/reddit_hero_mobile.png",
+                "hero_mobile_2x": "/assets/images/mobile/reddit_hero_mobile_2x.png",
+            },
+            "preview1": {
+                "preview1_desktop": "/assets/images/desktop/reddit_preview1_desktop.png",
+                "preview1_desktop_2x": "/assets/images/desktop/reddit_preview1_desktop_2x.png",
+                "preview1_tablet": "/assets/images/tablet/reddit_preview1_tablet.png",
+                "preview1_tablet_2x": "/assets/images/tablet/reddit_preview1_tablet_2x.png",
+                "preview1_mobile": "/assets/images/mobile/reddit_preview1_mobile.png",
+                "preview1_mobile_2x": "/assets/images/mobile/reddit_preview1_mobile_2x.png",
+            },
+            "preview2": {
+                "preview2_desktop": "/assets/images/desktop/reddit_preview2_desktop.png",
+                "preview2_desktop_2x": "/assets/images/desktop/reddit_preview2_desktop_2x.png",
+                "preview2_tablet": "/assets/images/tablet/reddit_preview2_tablet.png",
+                "preview2_tablet_2x": "/assets/images/tablet/reddit_preview2_tablet_2x.png",
+                "preview2_mobile": "/assets/images/mobile/reddit_preview2_mobile.png",
+                "preview2_mobile_2x": "/assets/images/mobile/reddit_preview2_mobile_2x.png",
+            }
+        }
+    },
+    {
+        "project_title": "Luxury Lives",
+        "project_index": "luxury-lives",
+        "project_description": <p>The goal was modernise a membership spa's site to a responsive design and to extend the branding from their logo consistenly throughout. I also wanted their unique selling point to be easy to understand and the value of membership to be immediately apparent with the way the prices are displayed.</p>,
+        "project_tags": "Mobile-First Development | Unit Testing",
+        "language_tags": "HTML | CSS | Javascript | React | Vitest",
+        "project_background": <p>The existing website needed modernising into a responsive design, the client wanted a clean and contemporary design using existing logo to build branding. I wanted the user to be able to quickly understand how membership worked and to quickly see comparitive value by including a key in the services list that emphasised the benefits of membership.</p>,
+        "project_code_url": "",
+        "project_live_url": "",
+        "image_paths": {
+            "thumbnail": {
+                "thumbnail_desktop": "/assets/images/desktop/luxury_lives_thumbnail_desktop.png",
+                "thumbnail_desktop_2x": "/assets/images/desktop/luxury_lives_thumbnail_desktop_2x.png",
+                "thumbnail_tablet": "/assets/images/tablet/luxury_lives_thumbnail_tablet.png",
+                "thumbnail_tablet_2x": "/assets/images/tablet/luxury_lives_thumbnail_tablet_2x.png",
+                "thumbnail_mobile": "/assets/images/mobile/luxury_lives_thumbnail_mobile.png",
+                "thumbnail_mobile_2x": "/assets/images/mobile/luxury_lives_thumbnail_mobile_2x.png",
+            },
+            "hero": {
+                "hero_desktop": "/assets/images/desktop/luxury_lives_hero_desktop.png",
+                "hero_desktop_2x": "/assets/images/desktop/luxury_lives_hero_desktop_2x.png",
+                "hero_tablet": "/assets/images/tablet/luxury_lives_hero_tablet.png",
+                "hero_tablet_2x": "/assets/images/tablet/luxury_lives_hero_tablet_2x.png",
+                "hero_mobile": "/assets/images/mobile/luxury_lives_hero_mobile.png",
+                "hero_mobile_2x": "/assets/images/mobile/luxury_lives_hero_mobile_2x.png",
+            },
+            "preview1": {
+                "preview1_desktop": "/assets/images/desktop/luxury_lives_preview1_desktop.png",
+                "preview1_desktop_2x": "/assets/images/desktop/luxury_lives_preview1_desktop_2x.png",
+                "preview1_tablet": "/assets/images/tablet/luxury_livespreview1__tablet.png",
+                "preview1_tablet_2x": "/assets/images/tablet/luxury_livespreview1__tablet_2x.png",
+                "preview1_mobile": "/assets/images/mobile/luxury_livespreview1__mobile.png",
+                "preview1_mobile_2x": "/assets/images/mobile/luxury_livespreview1__mobile_2x.png",
+            },
+            "preview2": {
+                "preview2_desktop": "/assets/images/desktop/luxury_lives_preview2_desktop.png",
+                "preview2_desktop_2x": "/assets/images/desktop/luxury_lives_preview2_desktop_2x.png",
+                "preview2_tablet": "/assets/images/tablet/luxury_lives_preview2_tablet.png",
+                "preview2_tablet_2x": "/assets/images/tablet/luxury_lives_preview2_tablet_2x.png",
+                "preview2_mobile": "/assets/images/mobile/luxury_lives_preview2_mobile.png",
+                "preview2_mobile_2x": "/assets/images/mobile/luxury_lives_preview2_mobile_2x.png",
+            }
+        }
+    },
+    {
+        "project_title": "Minimalist Portfolio",
+        "project_index": "minimalist-portfolio",
+        "project_description": <p>A Frontend Mentor design challenge to create a minimalist portfolio for a web developer.</p>,
+        "project_tags": "Mobile-First Development | Mobile",
+        "language_tags": "HTML | CSS | Javascript | React",
+        "project_background": <p>I have really enjoyed using Frontend Mentor to practice my skills. This challenge made sense for me to do at this point in my career and enjoyed using a data file in combination with React Router to create a simple way to add and retract projects from the portfolio in future.</p>,
+        "project_code_url": "https://github.com/callumskilling/minimalist-portfolio",
+        "project_live_url": "https://callumskilling.github.io/minimalist-portfolio/",
+        "image_paths": {
+            "thumbnail": {
+                "thumbnail_desktop": "/assets/images/desktop/minimalist_thumbnail_desktop.png",
+                "thumbnail_desktop_2x": "/assets/images/desktop/minimalist_thumbnail_desktop_2x.png",
+                "thumbnail_tablet": "/assets/images/tablet/minimalist_thumbnail_tablet.png",
+                "thumbnail_tablet_2x": "/assets/images/tablet/minimalist_thumbnail_tablet_2x.png",
+                "thumbnail_mobile": "/assets/images/mobile/minimalist_thumbnail_mobile.png",
+                "thumbnail_mobile_2x": "/assets/images/mobile/minimalist_thumbnail_mobile_2x.png",
+            },
+            "hero": {
+                "hero_desktop": "/assets/images/desktop/minimalist_hero_desktop.png",
+                "hero_desktop_2x": "/assets/images/desktop/minimalist_hero_desktop_2x.png",
+                "hero_tablet": "/assets/images/tablet/minimalist_hero_tablet.png",
+                "hero_tablet_2x": "/assets/images/tablet/minimalist_hero_tablet_2x.png",
+                "hero_mobile": "/assets/images/mobile/minimalist_hero_mobile.png",
+                "hero_mobile_2x": "/assets/images/mobile/minimalist_hero_mobile_2x.png",
+            },
+            "preview1": {
+                "preview1_desktop": "/assets/images/desktop/minimalist_preview1_desktop.png",
+                "preview1_desktop_2x": "/assets/images/desktop/minimalist_preview1_desktop_2x.png",
+                "preview1_tablet": "/assets/images/tablet/minimalist_preview1_tablet.png",
+                "preview1_tablet_2x": "/assets/images/tablet/minimalist_preview1_tablet_2x.png",
+                "preview1_mobile": "/assets/images/mobile/minimalist_preview1_mobile.png",
+                "preview1_mobile_2x": "/assets/images/mobile/minimalist_preview1_mobile_2x.png",
+            },
+            "preview2": {
+                "preview2_desktop": "/assets/images/desktop/minimalist_preview2_desktop.png",
+                "preview2_desktop_2x": "/assets/images/desktop/minimalist_preview2_desktop_2x.png",
+                "preview2_tablet": "/assets/images/tablet/minimalist_preview2_tablet.png",
+                "preview2_tablet_2x": "/assets/images/tablet/minimalist_preview2_tablet_2x.png",
+                "preview2_mobile": "/assets/images/mobile/minimalist_preview2_mobile.png",
+                "preview2_mobile_2x": "/assets/images/mobile/minimalist_preview2_mobile_2x.png",
+            }
+        }
+    }
+]
